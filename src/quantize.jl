ac400604c16de5ccc6bfff4a6efc7fdfca7bcd72
George Datseris <datseris.george@gmail.com>
2018-02-20 10:14:31 +0100

    average swing ratio rework

--- src/quantize.jl
+++ src/quantize.jl
@@ -1,11 +1,14 @@
 using MIDI, StatsBase
 export isgrid, classify, quantize, quantize!
+
+# Have to move swing stuff in different folder. Create module Jazz
+export average_swing_ratio, inbetween_portion
 ###############################################################################
 # Grid
 ###############################################################################
-function isgrid(Grid)
-    issorted(Grid) || throw(ArgumentError("Grids must be sorted."))
-    if Grid[1] != 0 || Grid[end] != 1
+function isgrid(grid)
+    issorted(grid) || throw(ArgumentError("Grids must be sorted."))
+    if grid[1] != 0 || grid[end] != 1
         throw(ArgumentError("Grids must start from 0 and end in 1."))
     end
     true
@@ -115,59 +118,60 @@ function quantize(notes::Notes, grid)
 end
 
 
+#### JAzz
+
 """
 ```julia
-average_swing_ratio(notes::Vector{Note}, tpq::Integer, asr_method::String)
+average_swing_ratio(notes::Notes, asr_method::String)
 ```
 Calculate the average swing ratio of given `notes` array. Return the average
 swing ratio and the associated standard deviation.
 ## Methods
-### `"AsrFromAll"`
-Classify each note with the "13triplet" method. Calculate for each
-swing note the swing ratio (with respect to a perfect quarter note)
-and then average for all notes.
-### `"Triplets"`
-Classify each note with the "triplets" method. Use the notes with
-id 3 to calculate the swing ratio, and the average for all.
+### `"swung8s"`
+Classify each note with the grid `[0, 2//3, 1]`. Use the notes with
+index 2 to calculate the swing ratio, and then average for all.
+### `"triplets"`
+Classify each note with the grud `[0, 1//3, 2//3, 1]`. Use the notes with
+index 3 to calculate the swing ratio, and then average for all.
 """
-function average_swing_ratio(notes::Vector{Note},
-  tpq::Integer, asr_method::String)
+function average_swing_ratio(notes::Notes, asr_method::String)
 
-  sr::Vector{Float64} = Float64[]
+    tpq = notes.tpq
+    sr = Float64[]
 
-  # Create array of Swing Notes
-  if asr_method == "AsrFromAll"
-    s = classify(notes, tpq, "13triplet")
-    swingnotes = notes[s .== 2]
-  elseif asr_method == "Triplets"
-    s = classify(notes, tpq, "triplets")
-    swingnotes = notes[s .== 3]
-  end
+    # Create array of Swing Notes
+    if asr_method == "swung8s"
+        s = classify(notes, [0, 2//3, 1])
+        swingnotes = notes[s .== 2]
+    elseif asr_method == "triplets"
+        s = classify(notes, [0, 1//3, 2//3, 1])
+        swingnotes = notes[s .== 3]
+    end
 
-  # Create Swing ratios array
-  for i in 1:length(swingnotes)
-    pos = Int64(swingnotes[i].position)
-    posmod = mod(pos, tpq)
-    push!(sr, posmod/(tpq-posmod))
-  end
+    # Create Swing ratios array
+    for i in 1:length(swingnotes)
+        pos = Int64(swingnotes[i].position)
+        posmod = mod(pos, tpq)
+        push!(sr, posmod/(tpq-posmod))
+    end
 
-  # asr /= length(swingnotes)
-  # # Calculate std
-  # for i in 1:length(swingnotes)
-  #   pos = Int64(swingnotes[i].position)
-  #   posmod = mod(pos, tpq)
-  #   sr = posmod/(tpq-posmod)
-  #   sr_std += (sr - asr)^2
-  # end
-  # sr_std = sqrt(sr_std/length(swingnotes)) # do not use length-1
+    # asr /= length(swingnotes)
+    # # Calculate std
+    # for i in 1:length(swingnotes)
+    #   pos = Int64(swingnotes[i].position)
+    #   posmod = mod(pos, tpq)
+    #   sr = posmod/(tpq-posmod)
+    #   sr_std += (sr - asr)^2
+    # end
+    # sr_std = sqrt(sr_std/length(swingnotes)) # do not use length-1
 
-  return mean_and_std(sr)
+    return mean_and_std(sr)
 end
 
 
 
 
-function inbetween_portion(notes::Vector{Note}, tpq)
-  clas = classify(notes, tpq, "triplets")
-  length(clas[clas .== 2])/length(notes)
+function inbetween_portion(notes::Notes)
+  clas = classify(notes, [0, 1//3, 2//3, 1])
+  count(i -> i == 2, clas)/length(notes)
 end
