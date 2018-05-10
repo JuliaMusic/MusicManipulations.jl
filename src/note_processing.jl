794c37f18bddf7392bff694ffb15d1dc17fa1446
George Datseris <datseris.george@gmail.com>
2018-05-10 15:19:40 +0200

    add firstnotes
    Does something similar to Lennart's function, but more general and based on a given grid

--- src/note_processing.jl
+++ src/note_processing.jl
@@ -1,32 +1,6 @@
 export getfirstnotes, purgepitches!, purgepitches, separatepitches, firstnotes
 
 """
-    getfirstnotes(notes::Notes, septicks = 960)
-
-Get only the first played note of each instrument in a series of notes.
-If no note is played for `septicks` ticks, the next following notes are
-considered a new series.
-"""
-function getfirstnotes(notes::Notes, trackno = 2, septicks = 960)
-    pitch = UInt8[] #save which pitches allready occurred in current series
-    firstnotes = Notes() #container for firstnotes
-
-    #iterate through all notes
-    for (i,note) in enumerate(notes)
-        #if the current note is the first of a new series, empty the pitches
-        if i>1 && note.position - notes.notes[i-1].position > septicks
-            pitch = UInt8[]
-        end
-        # take every first note of each pitch
-        if !(note.value in pitch)
-            push!(firstnotes.notes, note)
-            push!(pitch, note.value)
-        end
-    end
-    return firstnotes
-end
-
-"""
     firstnotes(notes, grid)
 
 Return the notes that first appear in each grid point, *without quantizing them*.
@@ -46,19 +20,6 @@ function firstnotes(notes::Notes{N}, grid) where {N<:AbstractNote}
 
     return Notes(toadd, notes.tpq)
 end
-
-    # prevtype = clas[1]
-    # j = 1 # previous note index
-    # i = 1 # current note index
-    #
-    # ret = [notes[1]]
-    #
-    # while i ≤ length(notes)
-    #     i += 1
-    #     # current note type
-    #     curtype = classify(notes[i], grid)
-    #     if curtype != prevtype
-    #         push!(ret, notes[i])
 
 
 
