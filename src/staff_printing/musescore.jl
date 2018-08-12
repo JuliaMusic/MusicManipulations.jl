b057f54e0235afae35405808ca3b0f8a86ce9031
George Datseris <datseris.george@gmail.com>
2018-08-12 17:45:02 +0100

    finalize musescore function.
    Too bad that this wont be tested on travis because it is too complicated.

--- src/staff_printing/musescore.jl
+++ src/staff_printing/musescore.jl
@@ -1,30 +1,40 @@
+export musescore
+
 using DefaultApplication
 
 const MUSESCORE = @static Sys.iswindows() ? "MuseScore" : "musescore"
 
-function musescore(file, notes::Notes;
-    tozero = true, display = true)
+function test_musescore()
+    r = run(`$(MUSESCORE) -v`)
+    @assert r.exitcode == 0
+end
 
-    @assert file[end-3:end] == ".png"
-    if tozero
-        shift = minimum(n.position for n in notes)
-        notes = translate(notes, -shift)
-    end
+"""
+    musescore(file, notes | midi; display = true)
+Use the open source software "MuseScore" to create a score and save the
+output to `file`. By default it will also display the created `file`,
+which can be either a `.pdf` or a `.png`.
 
-    dir =  dirname(file)
+Notice that MuseScore must be accessible from the command line for this function to
+work.
+"""
+function musescore(file, notes; display = true, grid = nothing)
+
+    test_musescore()
+    @assert file[end-3:end] ∈ (".png", ".pdf")
+
     tdir = tempdir()
     midi = writeMIDIfile(tdir*"/tempmid.mid", notes)
-
 	midipath = joinpath(tdir, "tempmid.mid")
-	@assert isfile(midipath)
 
-    cmd = `$MUSESCORE -n -T 10 -o $(file) $(midipath)`
+    if file[end-3:end] == ".png"
+        cmd = `$MUSESCORE -n -T 10 -o $(file) $(midipath)`
+        muspng = file[1:end-4]*"-1.png"
+    else
+        cmd = `$MUSESCORE -n -o $(file) $(midipath)`
+        muspng = file
+    end
 	run(cmd)
-
     rm(tdir*"/tempmid.mid")
-    pngname = basename(file)
-
-    muspng = file[1:end-4]*"-1.png"
-
     display && DefaultApplication.open(muspng)
 end
