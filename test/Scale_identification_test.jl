a64d4e64a726ed8fafb2c7bb1d85485a95d07320
Corentin Nélias <34754896+johncwok@users.noreply.github.com>
2019-02-07 11:24:52 +0100

    Update Scale_identification_test.jl

--- test/Scale_identification_test.jl
+++ test/Scale_identification_test.jl
@@ -1,4 +1,4 @@
 using Test
 
 tester = getnotes(readMIDIFile("recording_uwe_2.mid"),1)
-@test scale_identification(tester) == ("A# Major/G minor", ["C", "D", "D#", "F", "G", "A", "A#"])
+@test scale_identification(tester) == ("A♯ Major/G minor", ["C", "D", "D♯", "F", "G", "A", "A♯"])
