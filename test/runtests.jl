a8416709db855eeb711f71719ef39d4f51aeb954
George Datseris <datseris.george@gmail.com>
2019-02-09 10:48:17 +0100

    test scale identification

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,9 +4,8 @@ using Test
 
 cd(@__DIR__)
 
-include("Scale_identification_test.jl")
 include("midiio.jl")
 include("quantizer_tests.jl")
 include("timeseries.jl")
 include("data_extraction.jl")
-include("Scale_identification_test.jl")
+include("scale_tests.jl")
--- test/scale_tests.jl
+++ test/scale_tests.jl
@@ -1,4 +1,5 @@
 using Test
+using MusicManipulations: scale_identification
 
-tester = getnotes(readMIDIFile("recording_uwe_2.mid"),1)
-@test scale_identification(tester) == ("A♯ Major/G minor", ["C", "D", "D♯", "F", "G", "A", "A♯"])
+tester = getnotes(readMIDIFile(joinpath(@__DIR__, "recording_uwe_2.mid")),1)
+@test scale_identification(tester) == "A♯ Major/G minor"
