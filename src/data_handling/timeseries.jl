53596b88a1d708053e84d33f22c14785138e5962
Datseris <datseris.george@gmail.com>
2020-06-28 10:08:10 +0200

    add missingval option to timeseries

--- src/data_handling/timeseries.jl
+++ src/data_handling/timeseries.jl
@@ -1,7 +1,7 @@
 export timeseries, segment
 
 """
-    timeseries(notes::Notes, property::Symbol, f, grid; segmented = false) -> tvec, ts
+    timeseries(notes::Notes, property::Symbol, f, grid; kwargs...) -> tvec, ts
 Produce a timeseries of the `property` of the given notes, by
 first quantizing on the given `grid` (to avoid actual quantization
 use the grid `0:1//notes.tpq:1`). Return the time vector `tvec` in ticks
@@ -10,8 +10,8 @@ and the produced timeseries `ts`.
 After quantization, it is often the case that many notes are in the same bin of
 the grid. The **function** `f` denotes which value of the vector of the
 `property` of the notes to keep. Typical values are `minimum, maximum, mean`,
-etc. Notice that bins without any note in them obtain the value `missing`,
-regardless of the function `f` or the `property`.
+etc. Notice that bins without any note in them obtain the value of the keyword `missingval`,
+which be default is just `missing`, regardless of the function `f` or the `property`.
 
 If the `property` is `:velocity`, `:pitch`, or `:duration` the function
 behaves exactly as described. The `property` can also be `:position`.
@@ -19,12 +19,12 @@ In this case, the timeseries `ts` contain the timing deviations of the notes
 with respect to the `tvec` vector
 (these numbers are known as *microtiming deviations* in the literature).
 
-If 'segmented = true', the notes are segmented according to the grid in order to
-respect the information of their duration, see [`segment`](@ref).
+If given keyword 'segmented = true', the notes are segmented according to the grid
+in order to respect the information of their duration, see [`segment`](@ref).
 Otherwise the notes are treated as point events with no duration
 (it makes no sense to choose `:duration` with `segmented`).
 """
-function timeseries(notes, property, f, grid; segmented = false)
+function timeseries(notes, property, f, grid; segmented = false, missingval = missing)
     isgrid(grid)
     if segmented == true
         notes = segment(notes, grid)
@@ -35,7 +35,7 @@ function timeseries(notes, property, f, grid; segmented = false)
         property ∉ (:velocity, :pitch, :duration, :position, :channel)
         error("Unknown property!")
     end
-    ts, tvec, quantizedpos = _init_timeseries_vectors(notes, grid)
+    ts, tvec, quantizedpos = _init_timeseries_vectors(notes, grid, missingval)
     i = previdx = 1; L = length(quantizedpos); M = length(tvec)
     while i ≤ L
         # find entries of same grid bin
@@ -98,7 +98,7 @@ function produce_note_value(notes, property::Nothing, f, i, j)
     val = Float64(f(v))
 end
 
-function _init_timeseries_vectors(notes, grid)
+function _init_timeseries_vectors(notes, grid, missingval)
     tpq = notes.tpq
     qnotes = quantize(notes, grid)
     quantizedpos = positions(qnotes)
@@ -114,6 +114,7 @@ function _init_timeseries_vectors(notes, grid)
         append!(tvec, bins .+ c*tpq)
         c += 1
     end
-    ts = Vector{Union{Float64, Missing}}(undef, length(tvec))
+    ts = Vector{Union{Float64, typeof(missingval)}}(undef, length(tvec))
+    fill!(ts, missingval)
     return ts, tvec, quantizedpos
 end
