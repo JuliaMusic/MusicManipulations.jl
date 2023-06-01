18cc1d27655b0d838734a1873e266068c79fec38
NeroBlackstone <gf7600gs@gmail.com>
2023-06-01 23:06:03 +0800

    document change

--- src/namednote.jl
+++ src/namednote.jl
@@ -2,7 +2,7 @@ using MIDI: name_to_pitch, pitch_to_name, Note
 export NamedNote,NamedNotes
 
 """
-    NamedNote(name, pitch, velocity, position, duration, channel = 0) <: AbstractNote
+    NamedNote(name, velocity, position, duration, channel = 0) <: AbstractNote
 Mutable data structure describing a music note with certain pitch name. A bundle of many notes results
 in the [`Notes`](@ref) struct, which is the output of the [`getnotes`](@ref)
 function.
@@ -12,19 +12,31 @@ If the `channel` of the note is `0` (default), it is not shown.
 You can also create a `NamedNote` with the following keyword constructor:
 ```julia
 NamedNote(pitch_name::String; position = 0, velocity = 100, duration = 960, channel = 0)
+NamedNote(n::Note; pitch_name::String = "")
 ```
 
-Warning: We use attribute `pitch`,`duration`,`position`,`channel`,`velocity` to compare `AbstractNote` value equality. So:
+Initialize `Notes{NamedNote}` from a pitch name string, use spaces to separate pitch name.
+
+``` julia
+NamedNotes(notes_string::String; tpq::Int = 960)
+# NamedNotes("C#6 Db5 E4")
+```
+
+Converte `NamedNote` to `Note`:
+
+``` julia
+Note(n::NamedNote)
+```
+
+Warning: We use attribute `name_to_pitch(name)`,`duration`,`position`,`channel`,`velocity` to compare `NamedNote`. So:
 
 ``` julia
 NamedNote("Db5") == NamedNote("C#5")
+# true
 ```
 
 ## Fields:
-* `name::String` : Pitch name.
-* `pitch::UInt8` : Pitch, starting from C-1 = 0, adding one per semitone.
-  Use the functions [`name_to_pitch`](@ref) and
-  [`pitch_to_name`](@ref) for integer and string representations.
+* `name::String` : Pitch name, Uppercase.
 * `velocity::UInt8` : Dynamic intensity. Cannot be higher than 127 (0x7F).
 * `position::UInt` : Position in absolute time (since beginning of track), in ticks.
 * `duration::UInt` : Duration in ticks.
@@ -50,7 +62,7 @@ mutable struct NamedNote <: AbstractNote
             if !isnumeric(name[end])
                 name *= "5"
             end
-            @show name[prevind(name,end,1)]
+            
             # store flat use "♭", store sharp use "♯"
             if name[prevind(name,end,1)] == 'b'
                 name = replace(name,"b"=>"♭")
@@ -70,7 +82,7 @@ NamedNote(n::Note; pitch_name::String = "") =
 
 NamedNotes(notes_string::String; tpq::Int = 960) = Notes([NamedNote(String(s)) for s in split(notes_string," ")], tpq)
 
-Note(n::NamedNote) = Note(pitch_to_name(n.pitch), n.position, n.velocity, n.duration, n.channel)
+Note(n::NamedNote) = Note(name_to_pitch(n.pitch), n.position, n.velocity, n.duration, n.channel)
 
 function Base.show(io::IO, note::NamedNote) 
     nn = rpad(note.name, 3)
